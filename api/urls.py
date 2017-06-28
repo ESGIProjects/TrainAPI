d589bebec8c3ba81d0561025595e244929698793
Jason Pierna <siilver777@me.com>
2017-06-28 17:08:54 +0200

    Documentation interactive

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views as token_views
 
+from rest_framework_swagger.views import get_swagger_view
+
 from . import views
 
 router = DefaultRouter()
@@ -13,10 +15,13 @@ router.register(r'trains', views.TrainViewSet)
 router.register(r'stations', views.StationViewSet)
 router.register(r'users', views.UserViewSet)
 
-# Login and logout views for the browsable API
+# Swagger
+schema_view = get_swagger_view(title='TrainAPI')
+
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'login/', include('rest_framework.urls')),
     url(r'token/', token_views.obtain_auth_token),
-    url(r'^ratp/$', views.ratp_api_call)
+    url(r'^ratp/$', views.ratp_api_call),
+    url(r'^swagger/$', schema_view)
 ]
